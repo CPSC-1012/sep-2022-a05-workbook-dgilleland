80d50932128d15a181379554ce3010886d8cab54
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-09-14 16:11:58 -0600

    Math demos

--- src/003/Program.cs
+++ src/003/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// Create a program that will calculate the area and the perimeter of a square.
+int sideLength; // declare a variable
+sideLength = 2; // using a hard-coded value
+
+int area, perimeter; // You can declare more than one variable of a given type
+area = sideLength * sideLength;
+perimeter = sideLength + sideLength + sideLength + sideLength; // sideLength * 4
+
+// Example of string concatenation
+Console.WriteLine("The area of a square with length of " + sideLength + " is " + area);
+// Example of string interpolation
+Console.WriteLine($"The perimeter of a square with length of {sideLength} is {perimeter}");
+
+
---
+++ src/004/Program.cs
@@ -0,0 +1,13 @@
+﻿// declare our variables
+double radius, diameter, circumference, area;
+// Let's get the diameter from the user
+Console.Write("Enter a diameter: ");
+string temp; // declare a string for user input
+temp = Console.ReadLine(); // get a line of input from the user
+diameter = double.Parse(temp); // convert the string to a number
+// Radius = Diameter / 2
+radius = diameter / 2;
+Console.WriteLine($"A circle with that diameter will have a radius of {radius}.");
+circumference = Math.PI * diameter;
+Console.WriteLine($"The circumference is {circumference}");
+// Area = PI x radius^2
