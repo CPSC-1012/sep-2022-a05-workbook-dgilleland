1f90aaa179f13c2a59debe87609e3abb70d4a7e2
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-09-16 15:33:36 -0600

    Traditional Hello World

---
+++ src/TraditionalHelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿using System; // reference a namespace in an assembly
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
