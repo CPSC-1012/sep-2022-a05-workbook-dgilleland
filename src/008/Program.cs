9fd6cd02de03b1dcd0532d29c27cefd3847d3d29
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-09-21 21:17:19 +0000

    Sep 21, 2022, 3:17 PM

--- src/008/Program.cs
+++ src/008/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using static System.Console;
+using static System.Math;
+double tuition = 5500;
+WriteLine($"Basic tuition is {tuition:C}");
+
+
