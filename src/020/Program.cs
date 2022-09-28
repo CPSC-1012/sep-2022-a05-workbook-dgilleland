9a0009bca3b2340f9724aafc48b22af1aa194141
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-09-28 16:49:28 -0600

    Intro to traditional Program.Main()

---
+++ src/020/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _020;
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello, World!");
+    //  Namespace
+    //          Class
+    //                 Method
+        WriteLabel("The anwer is", 42.ToString());
+    }
+
+    static void WriteLabel(string label, string value)
+    {
+        ForegroundColor = ConsoleColor.Cyan;
+        Write(label);
+        ForegroundColor = ConsoleColor.DarkYellow;
+        WriteLine(value);
+        ResetColor();
+    }
+}
