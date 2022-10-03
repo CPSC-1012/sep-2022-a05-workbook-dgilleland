6929fbcbccfbda7c2d45dee5098cd32c50b37ec5
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-03 21:10:38 +0000

    Oct 3, 2022, 3:10 PM

--- src/021/Program.cs
+++ src/021/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Simple Looping Math");
+using static System.Console;
+WriteLine("Simple Looping Math");
+WriteLine("===================\n");
+
+// Methods
+static double PromptForNumber()
+{
+    // With simple validation
+    Write("Enter a number (leave empty to finish): ");
+    double result;
+    string userInput = ReadLine();
+    while(!double.TryParse(userInput, out result) && !string.IsNullOrEmpty(userInput))
+    {
+        ForegroundColor = ConsoleColor.DarkRed;
+        WriteLine($"The value {userInput} is not a number. Try again.");
+        ResetColor();
+        Write("Enter a number: ");
+        userInput = ReadLine();
+    }
+    return result;
+}
