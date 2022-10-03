5d4c4c83ba4494aa6735a82ebdbf40e4d232336d
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-03 21:25:10 +0000

    Oct 3, 2022, 3:25 PM

--- src/021/Program.cs
+++ src/021/Program.cs
@@ -1,7 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using static System.Console;
+
+// Main Method body
 WriteLine("Simple Looping Math");
 WriteLine("===================\n");
+
+double total = 0;
+int count = 0;
+double value;
+do
+{
+    value = PromptForNumber();
+    total += value;
+    count++;
+}while(value != 0);
+
+// QnD output
+WriteLine($"The count is {count} and the total is {total}");
 
 // Methods
 static double PromptForNumber()
