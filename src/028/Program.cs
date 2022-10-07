9d5bf5877dd870509a5e90a7b186ec56db9c75d4
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-07 22:37:41 +0000

    Oct 7, 2022, 4:37 PM

--- src/028/Program.cs
+++ src/028/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+using static System.Math;
+WriteLine("Potentially Useful Code");
+WriteLine("=======================\n");
+
+string shape;
+double area;
+
+WriteLine(@"Pick from the following shapes:
+
+\tT) Triangle
+\tS) Square
+\tR) Rectangle");
+// Laziness, thy name is Dan
+Write("Choose a shape: ");
