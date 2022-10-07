35d4bb2c96921b7a87f8a0c26ec1943f1e748fe8
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-07 22:40:42 +0000

    Oct 7, 2022, 4:40 PM

--- src/028/Program.cs
+++ src/028/Program.cs
@@ -13,3 +13,23 @@ WriteLine(@"Pick from the following shapes:
 \tR) Rectangle");
 // Laziness, thy name is Dan
 Write("Choose a shape: ");
+shape = ReadLine(); // We'll assume correct user input.
+char choice = shape[0]; // Ohhh, a string is just an array of characters!!
+choice = char.ToUpper(choice); // make it upper-case
+switch(choice)
+{
+    case 'T':
+        // TODO: Get the base and vertical height, then calculate the area
+        break;
+    case 'S':
+        // TODO: Get the side length, then calculate the area
+        break;
+    case 'R':
+        // TODO: Get the width and height, then calculate the area
+        break;
+    default:
+        area = 0;
+        break;
+}
+
+
