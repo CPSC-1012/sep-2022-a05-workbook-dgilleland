5815f8ff05eeca1616951bed2662dcf79be23682
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-12 22:09:09 +0000

    Oct 12, 2022, 4:09 PM

--- src/024/Program.cs
+++ src/024/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Simple Grid");
+
+for(int row = 0; row < 5; row++)
+{
+    for(int column = 0; column < 5; column++)
+    {
+        Write("X");
+    }
+    WriteLine(); // blank line
+}
