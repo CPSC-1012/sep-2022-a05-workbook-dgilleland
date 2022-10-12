be2c3a8048f23de285a0f87778da0c704c15fd71
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-12 21:33:55 +0000

    Oct 12, 2022, 3:33 PM

--- src/023/Program.cs
+++ src/023/Program.cs
@@ -16,7 +16,15 @@ while(!int.TryParse(temp, out number) || number <= 0)
 // Calculate the factorial
 int result = 1;
 for(int count = 1; count <= number; count++)
-    result = result * count;
+{
+    if(result <= int.MaxValue / count)
+        result = result * count; // do the math
+    else
+    {
+        WriteLine("That factorial result is too big to calculate");
+        break; // exit this for loop
+    }
+}
 
 WriteLine($"The result of {number}! is {result}");
 WriteLine($"By the way, the maximum integer value is {int.MaxValue}");
