39c98fdd5c3ba61d533b723601f232addffadc7c
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-12 21:22:11 +0000

    Oct 12, 2022, 3:22 PM

--- src/023/Program.cs
+++ src/023/Program.cs
@@ -5,7 +5,7 @@ string promptMessage = "Enter a positive whole number to find the factorial of t
 Write(promptMessage);
 string temp = ReadLine();
 int number;
-if(!int.TryParse(temp, out number) || number <= 0)
+while(!int.TryParse(temp, out number) || number <= 0)
 {
     // Reprompt
     WriteLine("That was not a positive whole number. Try again");
