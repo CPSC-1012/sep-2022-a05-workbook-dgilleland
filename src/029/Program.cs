cd0800486f37aad55b7cd1654e5d95fc051adcab
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-12 22:44:06 +0000

    Oct 12, 2022, 4:44 PM

--- src/029/Program.cs
+++ src/029/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+WriteLine("Learn Math\n");
+
+for(int row = 1; row <= 5; row++)
+{
+    for(int column = 1; column <=5; column++)
+    {
+        int product = row * column;
+        Write(product);
+    }
+    WriteLine();
+}
