937b8aa7e126e9249307e3d5da2a854b735d5a64
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-12 21:20:34 +0000

    Oct 12, 2022, 3:20 PM

--- src/023/Program.cs
+++ src/023/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+WriteLine("Factorial");
+// Get the user to enter a number
+string promptMessage = "Enter a positive whole number to find the factorial of that number: ";
+Write(promptMessage);
+string temp = ReadLine();
+int number;
+if(!int.TryParse(temp, out number) || number <= 0)
+{
+    // Reprompt
+    WriteLine("That was not a positive whole number. Try again");
+    Write(promptMessage);
+    temp = ReadLine();
+}
+
+// Calculate the factorial
+
