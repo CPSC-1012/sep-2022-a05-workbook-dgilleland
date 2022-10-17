00bc774b0f5a7b54a385c43d8357484bab8688c1
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-17 16:06:11 -0600

    Playing with Humanizer

--- src/030/MyApp/Program.cs
+++ src/030/MyApp/Program.cs
@@ -1,8 +1,30 @@
-﻿namespace Tutor;
+﻿using static System.Console;
+using Humanizer;
+namespace Tutor;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        WriteLine("Hello, Tutor!");
+
+        int number;
+        Write("Enter a number from 5 to 25: ");
+        while(!int.TryParse(ReadLine(), out number) || number < 5 || number > 25)
+        {
+            // Retry getting a good value from the user
+            WriteLine("That is not a valid number.");
+            Write("Enter a number from 5 to 25: ");
+        }
+        WriteLine($"Here are the numbers from 1 to {number}: ");
+        for(int count = 1; count <= number; count++)
+        {
+            WriteLine($"\t{count.ToOrdinalWords()}:   \t{count.ToWords()}");
+        }
+        WriteLine($"I paid {3800.ToWords()} (CAD) to learn how to do this.");
+
+        DateTime when;
+        when = DateTime.Now;
+        WriteLine($"The time is {when.Humanize()}");
+        WriteLine($"The buses leave {when.AddHours(1).AddMinutes(12).Humanize()}");
     }
 }
