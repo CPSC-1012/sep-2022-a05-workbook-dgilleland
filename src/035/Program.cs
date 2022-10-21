0456c0ddd5ceb9f57fa777a922f196521fc0d0c4
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-21 21:35:28 +0000

    Oct 21, 2022, 3:35 PM

--- src/035/Program.cs
+++ src/035/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using static System.Console; // Use the static members of the Console class
+                             // without having to write Console.  every time
+
+WriteLine("Hello, World!");
+WriteLine("Let's build a room.");
+
