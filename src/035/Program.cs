e214b90ca3f67cf5dc5206f8de94978b4e6042a7
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-21 21:46:17 +0000

    Oct 21, 2022, 3:46 PM

--- src/035/Program.cs
+++ src/035/Program.cs
@@ -5,3 +5,15 @@ using static System.Console; // Use the static members of the Console class
 WriteLine("Hello, World!");
 WriteLine("Let's build a room.");
 
+Rectangle backWall, leftWall, rightWall, frontWall;
+backWall = new(); // implicit
+backWall.Height = 5;
+backWall.Width = 10;
+
+frontWall = new Rectangle(); // explicit
+frontWall.Height = 5;
+frontWall.Width = 20;
+
+int paintableArea = backWall.Area
+                  + frontWall.Area;
+
