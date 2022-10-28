58cf2a6bbf5efae6f919111fc1b3dbad2e49d500
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-28 21:08:08 +0000

    Oct 28, 2022, 3:08 PM

--- src/033/Thermometer.cs
+++ src/033/Thermometer.cs
@@ -1,4 +1,13 @@
 public class Thermometer
 {
+    // An enumeration that only applies to temperatures
+    public enum Units { Celsius, Fahrenheit, Kelvin }
+    //                 \default/
+
+    public const int FreezingPointOfWater = 0;
+    public const double AbsoluteZero = -273.15;
+
     
+
+
 }
