15ea15ffcd4dc1f46caee96e39212b41e326002f
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-28 21:14:56 +0000

    Oct 28, 2022, 3:14 PM

--- src/033/Program.cs
+++ src/033/Program.cs
@@ -3,5 +3,9 @@
 WriteLine($"The lowest integer value is {int.MinValue}");
 WriteLine($"The freezing point of water is {Thermometer.FreezingPointOfWater}");
 
+Thermometer forMyDesk = new(); // Initialize our variable to a new object instance
+
+// Display our thermometer
+WriteLine($"My thermometer: {forMyDesk}");
 
 
