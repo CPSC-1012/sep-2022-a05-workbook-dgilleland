1513036a000efa9b0461e870c5d7ea49d790df6b
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-28 21:10:20 +0000

    Oct 28, 2022, 3:10 PM

--- src/033/Program.cs
+++ src/033/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine($"The lowest integer value is {int.MinValue}");
+
