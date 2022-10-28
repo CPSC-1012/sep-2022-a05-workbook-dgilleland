9491352af03b821668be25aaa2080cf7cfed6092
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-28 16:55:48 -0600

    End of today's demo

--- src/033/Program.cs
+++ src/033/Program.cs
@@ -1,4 +1,5 @@
 ﻿using static System.Console;
+using static Thermometer.Units;
 
 WriteLine($"The lowest integer value is {int.MinValue}");
 WriteLine($"The freezing point of water is {Thermometer.FreezingPointOfWater}");
@@ -7,5 +8,9 @@ Thermometer forMyDesk = new(); // Initialize our variable to a new object instan
 
 // Display our thermometer
 WriteLine($"My thermometer: {forMyDesk}");
-
+Write("Enter a temperature: ");
+forMyDesk.CurrentTemperature = double.Parse(ReadLine());
+WriteLine($"You entered {forMyDesk.ToString()}");
+WriteLine($"For you old people, that is {forMyDesk.ToString(Fahrenheit)}");
+WriteLine($"For you sci-fi-folk, that is {forMyDesk.ToString(Kelvin)}");
 
--- src/033/Thermometer.cs
+++ src/033/Thermometer.cs
@@ -8,13 +8,66 @@ public class Thermometer
     public const double AbsoluteZero = -273.15;
 
     // Instance members of the class
-    public double CurrentTemperature;
+    private double _CurrentTemperature; // This field is our "backing store"
+    public double CurrentTemperature
+    {
+        get
+        {
+            return _CurrentTemperature; // Return the value from our backing store
+        }
+        set
+        {
+            // Here is a good place to VALIDATE the information coming in
+            if(value < AbsoluteZero)
+                throw new Exception($"Impossible temperature: {value} is less than absolute zero {AbsoluteZero}");
+            _CurrentTemperature = value; // Store the incoming value in our field
+        }
+    }
 
+    // Method to adjust the temperature
+    public void Adjust(double amount)
+    {
+        // Notice how our property is accessible to all methods of our class
+        CurrentTemperature += amount;
+    }
 
     // In the following method, I change the default way
     // my object instances get turned into "strings"
     public override string ToString()
     {
         return $"{CurrentTemperature}{'\u00B0'} {Units.Celsius}";
+    }
+
+    public string ToString(Units targetUnit)
+    {
+        string result = "";
+        double temp;
+        switch(targetUnit)
+        {
+            case Units.Celsius:
+                temp = CurrentTemperature;
+                break;
+            case Units.Fahrenheit:
+                temp = ToFahrenheit();
+                break;
+            case Units.Kelvin:
+                temp = ToKelvin();
+                break;
+            default:
+                temp = 0;
+                break;
+        }
+        result = $"{temp}{'\u00B0'} {targetUnit}";
+        return result;
+    }
+    public double ToKelvin()
+    {
+        return CurrentTemperature + (-1 * AbsoluteZero);
+    }
+    public double ToFahrenheit()
+    {
+        double result;
+        result = (CurrentTemperature * 9.0/5.0) + 32;
+        return result;
     }
 }
