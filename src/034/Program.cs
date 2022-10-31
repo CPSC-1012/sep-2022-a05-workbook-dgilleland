424c5200ea1d0b2414c4536f41fdf5386d4a1561
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-31 21:31:15 +0000

    Oct 31, 2022, 3:31 PM

--- src/034/Program.cs
+++ src/034/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using static System.Console;
 Console.WriteLine("Hello, World!");
+
+
+// Methods
+static void Display(string[] info)
+{
+    if(info.Length > 0)
+    {
+        for(int index = 0; index < info.Length; index ++)
+            WriteLine($"[{index}]  \"{info[index]}");
+            // e.g.        [0]           "Text"
+    }
+    else
+        WriteLine("The array is empty.");
+}
