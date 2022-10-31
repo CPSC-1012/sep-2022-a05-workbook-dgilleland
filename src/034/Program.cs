b77c96d912bd00c52821f844762879d370119bda
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-10-31 21:33:10 +0000

    Oct 31, 2022, 3:33 PM

--- src/034/Program.cs
+++ src/034/Program.cs
@@ -1,5 +1,6 @@
 ﻿using static System.Console;
-Console.WriteLine("Hello, World!");
+Console.WriteLine("This is what was passed in:");
+Display(args); // our string[] args
 
 
 // Methods
@@ -8,7 +9,7 @@ static void Display(string[] info)
     if(info.Length > 0)
     {
         for(int index = 0; index < info.Length; index ++)
-            WriteLine($"[{index}]  \"{info[index]}");
+            WriteLine($"[{index}]  \"{info[index]}\"");
             // e.g.        [0]           "Text"
     }
     else
