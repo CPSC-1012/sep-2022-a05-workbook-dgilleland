da0db82511be6f7bbc545831619405f2c3e59236
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-02 18:49:48 +0000

    Nov 2, 2022, 12:49 PM

--- src/042/Program.cs
+++ src/042/Program.cs
@@ -1,7 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
 
+WriteLine("Yadda Yahtzee!");
 
+int count = 0;
+int targetRoll;
+Write("What number (1-6) do you want to attempt to roll? ");
+targetRoll = PromptNumber(1, 6);
 
 // use of = instead of ==
 // Semicolon at end of if statement
@@ -10,4 +14,10 @@ Console.WriteLine("Hello, World!");
 // int roll = 0;
 // while(roll > 0 && roll < max)
 
-
+int PromptNumber(int min, int max)
+{
+    int roll = 0;
+    while(!int.TryParse(ReadLine(), out roll) || roll < min || roll > max);
+        Write("That was not a value between {min} and {max}. Try again: ");
+    return roll;
+}
