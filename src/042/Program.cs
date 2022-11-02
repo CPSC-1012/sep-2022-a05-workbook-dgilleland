b21b375ec20ae86be1e16b94e11a1434df662e9e
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-02 18:59:01 +0000

    Nov 2, 2022, 12:59 PM

--- src/042/Program.cs
+++ src/042/Program.cs
@@ -1,19 +1,26 @@
 ﻿using static System.Console;
 
 WriteLine("Yadda Yahtzee!");
-
+Die aDie = new();
 int count = 0;
 int targetRoll;
 Write("What number (1-6) do you want to attempt to roll? ");
 targetRoll = PromptNumber(1, 6);
 
-// use of = instead of ==
-// Semicolon at end of if statement
-// Semicolon at end of while
-// Forgetting the $ on the string
-// int roll = 0;
-// while(roll > 0 && roll < max)
+WriteLine("\nAttempting up to 10 rolls to get that number...");
+while(count > 0 && count < 10 || targetRoll != aDie.FaceValue)
+{
+    aDie.Roll();
+    count++;
+}
 
+if(aDie.FaceValue == targetRoll)
+    WriteLine($"You got the roll {targetRoll} in {count} attempts.");
+else
+    WriteLine("Sorry, you did not get the {targetRoll}");
+
+
+// Methods
 int PromptNumber(int min, int max)
 {
     int roll = 0;
