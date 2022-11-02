f878dcc1601321586697af5289d577a183a1ff4f
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-02 18:40:58 +0000

    Nov 2, 2022, 12:40 PM

--- src/042/Program.cs
+++ src/042/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+// use of = instead of ==
+// Semicolon at end of if statement
+// Semicolon at end of while
+// Forgetting the $ on the string
+// int roll = 0;
+// while(roll > 0 && roll < max)
+
+static int Roll(int max)
+{
+    return Random.
+}
