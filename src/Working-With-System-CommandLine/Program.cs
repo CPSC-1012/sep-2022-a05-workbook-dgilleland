1653706b3e7c95093592080c8c902cf1ab7c7e61
Dan Gilleland <dgilleland@nait.ca>
2022-11-03 14:52:21 -0600

    Playing with modern CLI tools

---
+++ src/Working-With-System-CommandLine/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.CommandLine;
+
+Option<FileInfo?> fileOption = new (
+    name: "--file",
+    description: "The file to read and display on the console.");
+
+RootCommand rootCommand = new("Sample app for System.CommandLine");
+rootCommand.AddOption(fileOption);
+
+rootCommand.SetHandler((file) => 
+    { 
+        ReadFile(file!); 
+    },
+    fileOption);
+
+await rootCommand.InvokeAsync(args);
+
+static void ReadFile(FileInfo file)
+{
+    File.ReadLines(file.FullName).ToList()
+        .ForEach(line => Console.WriteLine(line));
+}
---
+++ src/Working-With-System-CommandLine/ReadMe.md
@@ -0,0 +1,6 @@
+# Working With System.CommandLine
+
+Articles
+
+- [Demystifying System.CommandLine](https://intellitect.com/blog/demystified-system-commandline/)
+- [Tutorial: Get started with System.CommandLine](https://learn.microsoft.com/en-us/dotnet/standard/commandline/get-started-tutorial)
