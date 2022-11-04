a1455fded12845e546d7bcb66ebcf1dbe138c34b
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-04 23:09:25 +0000

    Nov 4, 2022, 5:09 PM

--- src/MakingMethods/Program.cs
+++ src/MakingMethods/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+// Let's create a program that will compare the areas of two shapes and identify which is bigger.
+// - The user can identify which shape they want: Circle, Square, Triangle, or Rectangle
+// - Formulas:
+//      - Circle: Area = PI * r^2
+//      - Square: Area = side ^ 2
+//      - Triangle: Area = 1/2 * base * height
+//      - Rectangle: Area = length * width
+// - The user can repeat the action of comparing shapes, pressing "X" to exit
+// - Validate all user input
+// - Handle all dimensions as double data types
+
