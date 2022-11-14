1fbc3e9485c46c3863a43d962a4509cc5d4a84ce
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-14 23:26:35 +0000

    Nov 14, 2022, 4:26 PM

--- src/GameLibrary/App/Program.cs
+++ src/GameLibrary/App/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+using Games; // DropDeadGame
+using Games.CommonObjects; // Die
+
+WriteLine("!! Ad-hoc testing of DropDeadGame !!");
+
+// Make a bunch of dice
+Die[] testDie = new Die[5];
+// Create each die object in the array
+for(int index = 0; index < testDie.Length; index ++)
+    testDie[index] = new Die();
+
+// Try out my DropDeadGame
+string[] players = new string[] {"Dan", "Bob" };
+DropDeadGame myGame = new DropDeadGame(players);
