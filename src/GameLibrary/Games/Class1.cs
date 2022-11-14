ed588a1b7db1911a1b8536250cdeb3cdad4906ec
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-14 22:59:37 +0000

    Nov 14, 2022, 3:59 PM

--- src/GameLibrary/Games/Class1.cs
+++ src/GameLibrary/Games/Class1.cs
@@ -1,5 +1,9 @@
 ﻿namespace Games; // New way to declare a namespace for the following types/classes.
 public class DropDeadGame
 {
-
+    // Have an array of strings for player names
+    private readonly string[] PlayerNames;
+    // Have an array of integers for player scores
+    private readonly int[] PlayerScores;
+    
 }
