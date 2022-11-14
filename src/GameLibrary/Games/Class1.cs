6ecaa9f23d0536878b2f2c468989c7bcefe2d85b
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-14 23:04:57 +0000

    Nov 14, 2022, 4:04 PM

--- src/GameLibrary/Games/Class1.cs
+++ src/GameLibrary/Games/Class1.cs
@@ -5,5 +5,16 @@ public class DropDeadGame
     private readonly string[] PlayerNames;
     // Have an array of integers for player scores
     private readonly int[] PlayerScores;
-    
+    // I should have a constructor that gets the players
+    public DropDeadGame(string[] players)
+    {
+        // TODO: Validation - if there's a problem, I'll be throwing an exception
+        //  - Not enough players for the game (min 2)
+        //  - Make sure the individual names are not "empty"
+
+        PlayerNames = players;
+        PlayerScores = new int[players.Length]; // an array the same size as the # of players
+        // Each element in the array has the default value for the int data type
+        // (which is zero)
+    }
 }
