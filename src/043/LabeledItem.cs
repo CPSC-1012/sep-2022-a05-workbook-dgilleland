9b7e7c288399cbc69a07af57a943b511c3dffc69
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-16 23:21:18 +0000

    Nov 16, 2022, 4:21 PM

--- src/043/LabeledItem.cs
+++ src/043/LabeledItem.cs
@@ -1,8 +1,18 @@
-public class LabeledItem<T> // The <T> is my generic data type
+// The <T> is my generic data type
+public class LabeledItem<T> : ILabel
 {
     // The data type of the item is <T>
     public T Item { get; set; }
-    public readonly string Label;
+    public string Label { get; private set; } // Implements the ILabel.Label property
+    public string GetValue()
+    {
+        string result;
+        if(Item == null)
+            result = "-";
+        else
+            result = Item.ToString();
+        return result;
+    }
 
     // Constructor
     public LabeledItem(string text, T value)
