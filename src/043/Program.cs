7507c59ff2385fda5b1e386eeeef9d0313d8b5dd
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-16 22:14:32 +0000

    Nov 16, 2022, 3:14 PM

--- src/043/Program.cs
+++ src/043/Program.cs
@@ -1,3 +1,13 @@
 ﻿using static System.Console;
 Clear();
 WriteLine("Assorted Useful Things...\n");
+
+string[] languages = new string[] {"C#", "SQL", "HTML", "JavaScript", "CSS"};
+// Traditional for loop to go through the array
+for(int index = 0; index < languages.Length; index++)
+{
+    // Using a simple variable to reference one of the items in the array
+    string programmingLanguage = languages[index];
+    // do something with it...
+    WriteLine($"I know {programmingLanguage.ToLower()}.");
+}
