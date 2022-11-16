5222005e9f78d70dbd19fe9a96592eb56c487f8a
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-16 23:14:28 +0000

    Nov 16, 2022, 4:14 PM

--- src/043/ILabel.cs
+++ src/043/ILabel.cs
@@ -1,5 +1,8 @@
+// An interface defines a "contract" of a set of methods/properties
+// that any class can choose to implement.
+
 public interface ILabel
 {
-    string Label {get;}
-    object Value {get;}
+    string Label {get;} // A property that has to be implemented
+    string GetValue();  // A method that has to be implemented
 }
