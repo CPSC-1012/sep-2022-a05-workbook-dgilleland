dd5670246c90a0c6201c133cece72ecae51386a7
Dan Gilleland <DGilleland@nait.ca>
2022-11-18 13:57:37 -0700

    Added title + moved main code to a driver method

--- src/GameLibrary/App/Program.cs
+++ src/GameLibrary/App/Program.cs
@@ -4,23 +4,29 @@ using Games; // DropDeadGame
 using Games.CommonObjects; // Die
 
 Clear();
-WriteRule("Drop-Dead Game Demo");
-List<string> players = new();
-string input;
-do
+Title = "Game Library";
+DropDeadDriver();
+
+
+void DropDeadDriver()
 {
-    input = Prompt("Enter a player name (blank to stop):");
-    if(!string.IsNullOrWhiteSpace(input))
-        players.Add(input.Trim());
-}while(!string.IsNullOrWhiteSpace(input));
+    WriteRule("Drop-Dead Game Demo");
+    List<string> players = new();
+    string input;
+    do
+    {
+        input = Prompt("Enter a player name (blank to stop):");
+        if (!string.IsNullOrWhiteSpace(input))
+            players.Add(input.Trim());
+    } while (!string.IsNullOrWhiteSpace(input));
 
-DropDeadGame game = new(players.ToArray());
-game.TurnStarted += DisplayTurnStart;
-game.RollFinished += DisplayTurnResults;
-WriteLine("Drop-Dead Game Results");
-WriteLine("----------------------");
-game.Play();
-
+    DropDeadGame game = new(players.ToArray());
+    game.TurnStarted += DisplayTurnStart;
+    game.RollFinished += DisplayTurnResults;
+    WriteLine("Drop-Dead Game Results");
+    WriteLine("----------------------");
+    game.Play();
+}
 void DisplayTurnStart(object? sender, string e)
 {
     WriteLine($"{e}'s rolls:");
@@ -30,7 +36,7 @@ void DisplayTurnStart(object? sender, string e)
 void DisplayTurnResults(object? sender, DropDeadTurnResult e)
 {
     string rolls = string.Join(", ", e.DieRolls);
-    WriteLine($"    {e.CurrentScore, 5} | {rolls}");
-    if(e.TurnStatus == TurnStatus.Finished)
+    WriteLine($"    {e.CurrentScore,5} | {rolls}");
+    if (e.TurnStatus == TurnStatus.Finished)
         WriteLine($"{e.PlayerName}'s Score: {e.CurrentScore}\n");
 }
