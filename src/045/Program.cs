48b3bee28da94eeac5561682ca87bbbd2cffdf2f
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-23 22:34:25 +0000

    Nov 23, 2022, 3:34 PM

--- src/045/Program.cs
+++ src/045/Program.cs
@@ -7,7 +7,7 @@ try
     MyDriver driver = new();
     driver.Run();
 }
-catch (System.Exception ex)
+catch (System.Exception ex) // I can have from zero to many catch blocks
 {
     ForegroundColor = ConsoleColor.DarkRed;
     WriteLine("Looks like there's a problem my program wasn't expecting. Time for me to exit");
