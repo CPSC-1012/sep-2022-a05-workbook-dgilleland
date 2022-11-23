eacf8f3005d755fe9df55eacf3130799918b076c
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-23 22:36:58 +0000

    Nov 23, 2022, 3:36 PM

--- src/045/Program.cs
+++ src/045/Program.cs
@@ -4,9 +4,13 @@ using static System.Console;
 
 try
 {
+    // The try block is where the code we plan on using
+    // (and hope is working correctly) is executed.
     MyDriver driver = new();
     driver.Run();
 }
+// Catch blocks are where we can intercept a thrown exception (problem)
+// and deal with appropriately
 catch (System.Exception ex) // I can have from zero to many catch blocks
 {
     ForegroundColor = ConsoleColor.DarkRed;
@@ -15,5 +19,7 @@ catch (System.Exception ex) // I can have from zero to many catch blocks
 }
 finally
 {
+    // The finally block is where we can do some "clean-up" for our program.
+    // The finally block will always run, whether or not there is an exception.
     ResetColor();
 }
