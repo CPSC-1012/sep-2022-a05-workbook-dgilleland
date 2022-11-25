d89d7bf98fc9144a34fbcbbd3cb3a13c3e0860f7
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-25 22:34:00 +0000

    Nov 25, 2022, 3:34 PM

--- src/046/Program.cs
+++ src/046/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string message = @"Hello, World!
+string message = @$"Hello, World!
 
 This is a good day to (do a lab).
+Today is {DateTime.Today.ToLongDateString()}.
 ";
 File.WriteAllText("AdHoc.log", message);
