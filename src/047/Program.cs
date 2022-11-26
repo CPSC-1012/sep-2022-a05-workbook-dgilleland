be70e1134af121a0ba48347e3e2e88788dbd0ad8
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-26 00:21:44 +0000

    Nov 25, 2022, 5:21 PM

--- src/047/Program.cs
+++ src/047/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args) // Entry Point
+    {
+        // In a Top-Level Program, you basically have only the "body" of
+        // this Main method
+        Console.WriteLine("Hello, World!");
+        
+        // Take an OOP-view of my primitive string data type
+        string name; // The terms "variable" and "object" are essentially the same thing
+        name = new string("Dan Gilleland");
+
+        // Initialization Statement: declare my variable AND give it a value
+        int characterCount = name.Length;
+        //                       ^Field^
+
+        bool isInstructor = name.EndsWith("Gilleland");
+        //                      ^----- Method -------^  Calling the method of my variable/object
+    }
+}
