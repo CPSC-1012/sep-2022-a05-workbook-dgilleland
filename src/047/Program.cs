b294f785d9b62727d4c9ca19f6e952791f5a517f
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-26 00:45:18 +0000

    Nov 25, 2022, 5:45 PM

--- src/047/Program.cs
+++ src/047/Program.cs
@@ -8,8 +8,13 @@
         
         // Take an OOP-view of my primitive string data type
         string name; // The terms "variable" and "object" are essentially the same thing
+        //     v----- Intantiate --------v
         name = new String("Dan Gilleland");
         //         ^--- Constructor -----^
+        // I am calling the constructor in order
+        // to create my new String object
+        // and then I store (= is the "assignment operator")
+        // that object in my variable (left-hand side of the assignment operator)
 
         // Initialization Statement: declare my variable AND give it a value
         int characterCount = name.Length;
@@ -17,5 +22,16 @@
 
         bool isInstructor = name.EndsWith("Gilleland");
         //                      ^----- Method -------^  Calling the method of my variable/object
+
+        // ******************* //
+
+        MyString friendName;
+        //           v----- Intantiate ------v
+        friendName = new MyString("Robin Law");
+        //               ^--- Constructor ---^
+
+        characterCount = friendName.Length;
+        //                         ^Field^
+        
     }
 }
