3d7b3942a73f127c5b91995aa003d1c9d32dec24
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-29 00:32:34 +0000

    Nov 28, 2022, 5:32 PM

--- src/048/Driver.cs
+++ src/048/Driver.cs
@@ -1,5 +1,10 @@
 // See https://aka.ms/new-console-template for more information
 public class Driver
 {
-    
+    internal void Run()
+    {
+        // A) List the contents of this directory
+        // B) Open a file to read the contents
+        // C) Output the contents to the console.
+    }
 }
