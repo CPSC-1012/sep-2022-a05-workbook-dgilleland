73af030396dc4819aa08c1404b951b0676703865
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-11-29 00:39:07 +0000

    Nov 28, 2022, 5:39 PM

--- src/048/Driver.cs
+++ src/048/Driver.cs
@@ -1,9 +1,23 @@
-// See https://aka.ms/new-console-template for more information
+using System.IO; // for file Input/Output classes
+using static System.Console;
 public class Driver
 {
     internal void Run()
     {
         // A) List the contents of this directory
+        string[] folderContents = Directory.GetDirectories(".");
+        WriteLine($"There are {folderContents.Length} directories in the current directory.");
+        for(int index = 0; index < folderContents.Length; index ++)
+        //      ^-start-^  ^--go until the end of array-^
+        {
+            // Get the item in the array
+            string item = folderContents[index];
+            //                          ^-pos-^
+            // do something with it....
+            string[] temp = Directory.GetDirectories(item);
+            int count = temp.Length;
+            WriteLine($"\t{count} items in the folder `{item}`");
+        }
         // B) Open a file to read the contents
         // C) Output the contents to the console.
     }
