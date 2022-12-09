fa064cc5e078c949709b4e562b11bd59ffb4a111
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-12-09 23:00:08 +0000

    Dec 9, 2022, 4:00 PM

--- src/050/Program.cs
+++ src/050/Program.cs
@@ -2,4 +2,10 @@
 
 WriteLine("Hello, World!");
 
+Company myOldCompany = 
+    new Company("Dynamic Delivery", "Edmonton", new DateTime(1992, 1, 12));
+
+Company currentBusiness = 
+    new Company("Dynamic Generation Inc.", "Edmonton", new DateTime(2005, 2, 15), true);
+
 
