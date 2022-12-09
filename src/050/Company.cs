d95881b8d4590ef61e574dfe09b13bae6bef29b7
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-12-09 22:50:28 +0000

    Dec 9, 2022, 3:50 PM

--- src/050/Company.cs
+++ src/050/Company.cs
@@ -1,6 +1,11 @@
 public class Company
 {
     #region Properties and Fields
+    public string Name { get; set; }
+    public string City { get; set; }
+    public bool IsIncorpated { get; set; }
+    public DateTime BusinessStartDate { get; private set; }
+    public decimal? GrossIncomeToDate { get; private set; }
     #endregion
 
     #region Constructor(s)
