c8c5a74e17f9ce5ce19037c0ec37f1a92d5570af
Dan Gilleland <dgilleland@users.noreply.github.com>
2022-12-09 17:21:29 -0700

    Company w. Dates example

--- src/050/Company.cs
+++ src/050/Company.cs
@@ -1,3 +1,4 @@
+using static MonthName;
 public class Company
 {
     #region Properties and Fields
@@ -6,6 +7,12 @@ public class Company
     public bool IsIncorpated { get; set; }
     public DateTime BusinessStartDate { get; private set; }
     public decimal? GrossIncomeToDate { get; private set; }
+
+    public MonthName YearEndMonth { get; private set; } = MonthName.Dec;
+    //     ^-------^ ^----------^                         ^-----------^
+    //       |          |_ Variable                         |_ Value
+    //       |_ Data Type
+    public int YearEndDay {get; private set; } = 31;
     #endregion
 
     #region Constructor(s)
@@ -28,6 +35,60 @@ public class Company
             GrossIncomeToDate += earnings;
         else
             GrossIncomeToDate = earnings;
+    }
+
+    public bool IsOpen(DateTime onDate)
+    {
+        bool open = true;
+        // Closed on the weekend!
+        if(onDate.DayOfWeek == DayOfWeek.Saturday || onDate.DayOfWeek == DayOfWeek.Sunday)
+            open = false;
+
+        return open;
+    }
+
+    public void SetFiscalYearEnd(MonthName month, int day)
+    {
+        // Perform validation in a private method
+        Validate(month, day);
+        YearEndDay = day;
+        YearEndMonth = month;
+    }
+
+    public int DaysRemainingInFiscalYear(DateTime fromDate)
+    {
+        int count = -1; // initial value is just to get red squiggles removed
+        int endMonth = (int) YearEndMonth; // casting to get the underlying int value
+        int sameFiscalYear = fromDate.Year;
+        if(fromDate.Month > endMonth || (fromDate.Month == endMonth && fromDate.Day >= YearEndDay))
+            sameFiscalYear ++; // add one
+
+        DateTime prospectiveYearEnd = new DateTime(sameFiscalYear, endMonth, YearEndDay);
+
+        TimeSpan difference = prospectiveYearEnd - fromDate;
+        //       ^--------^   ^----------------^   ^------^
+        //        TimeSpan         DateTime      - DateTime
+
+        // Note: I lose a "fraction" of a day, but that's not important for this method
+        count = (int) difference.TotalDays;
+        return count;
+    }
+
+
+    private void Validate(MonthName month, int day)
+    {
+        // See https://www.timeanddate.com/calendar/months/
+        // bool isBadMonthAndDay = true;
+        bool isValidMonthAndDay = false;
+
+        // Try to prove it is an acceptable month/day
+        if(day <= 28 && month == Feb)
+            isValidMonthAndDay = true;
+        else if(day <= 31 && (month == Jan || month == Mar || month == May || month == Jul || month == Aug || month == Oct || month == Dec))
+            isValidMonthAndDay = true;
+        else if(day <= 30 && month != Unknown)
+            isValidMonthAndDay = true;
+        if(!isValidMonthAndDay) throw new ArgumentOutOfRangeException();
     }
 
     public override string ToString()
---
+++ src/050/MonthName.cs
@@ -0,0 +1,3 @@
+// Unknown is equal to 0
+// Jan = 1, Feb = 2, etc., which corresponds with valid month numbers in DateTime
+public enum MonthName { Unknown, Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec }
